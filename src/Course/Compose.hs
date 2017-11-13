072e5d32a6cb8afd17b80a2a576a0e01a0c2f0d2
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-11-13 18:06:00 +0300

    Complete Extend, Comonad and Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Course.Compose where
 
@@ -16,20 +17,27 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) ::
+    (a -> b)
+    -> Compose f g a
+    -> Compose f g b
+  fn <$> (Compose f) =
+    Compose ((fn <$>) <$> f)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
--- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  pure ::
+    a -> Compose f g a
+  pure a =
+    Compose (pure (pure a))
+  (<*>) ::
+    Compose f g (a -> b)
+    -> Compose f g a
+    -> Compose f g b
+  (Compose fns) <*> (Compose f) =
+    Compose (lift2 (<*>) fns f)
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
--- Implement the (=<<) function for a Monad instance for Compose
   (=<<) =
     error "todo: Course.Compose (<<=)#instance (Compose f g)"
--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -33,8 +33,8 @@ instance Extend ExactlyOne where
     (ExactlyOne a -> b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance ExactlyOne"
+  f <<= a =
+    ExactlyOne (f a)
 
 -- | Implement the @Extend@ instance for @List@.
 --
@@ -51,8 +51,8 @@ instance Extend List where
     (List a -> b)
     -> List a
     -> List b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance List"
+  _ <<= Nil = Nil
+  f <<= (a:.as) = f (a:.as) :. (f <<= as)
 
 -- | Implement the @Extend@ instance for @Optional@.
 --
@@ -66,8 +66,9 @@ instance Extend Optional where
     (Optional a -> b)
     -> Optional a
     -> Optional b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Optional"
+  _ <<= Empty = Empty
+  f <<= oa =
+    Full (f oa)
 
 -- | Duplicate the functor using extension.
 --
@@ -87,4 +88,4 @@ cojoin ::
   f a
   -> f (f a)
 cojoin =
-  error "todo: Course.Extend#cojoin"
+  (<<=) id
